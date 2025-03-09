73c13458efdca7a7a9525fe2628f49a4cf7b9338
Chao Zhang <chao.zhangg.zc@gmail.com>
2025-03-10 07:36:24 +0800

    UI: Add key descriptions

--- app/src/main/java/org/example/ui/MainWindow.java
+++ app/src/main/java/org/example/ui/MainWindow.java
@@ -5,7 +5,8 @@ import org.example.domain.GameMap;
 
 public class MainWindow {
 
-    private static final String RENDER_TEMPLATE = "Player: %s | Exp: %s\n";
+    private static final String RENDER_TEMPLATE =
+            "Player: %s | Exp: %s\nMove: W, A, S, D | Fight: F | Send command: Enter\n";
 
     private Map map = new Map();
 
